2188986a5db36b877ab00370c84c8e222ba69303
Anton Trafimovich <tonytrof@gmail.com>
2024-09-12 11:20:23 +0200

    Added get user by session method to session service.

--- packages/api/src/service/session-service.js
+++ packages/api/src/service/session-service.js
@@ -6,4 +6,13 @@ export default {
   createSession: async (db, userId) => {
     return db.sessions.createSession(userId);
   },
+
+  getUserBySession: async (db, sessionId) => {
+    const session = await db.sessions.getSessionById(sessionId);
+    if (!session) {
+      return null;
+    }
+
+    return db.users.getUserById(session.userId);
+  },
 };
