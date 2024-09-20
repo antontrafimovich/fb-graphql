e378a08844018b72e02220ad2836702ea480b866
Anton Trafimovich <tonytrof@gmail.com>
2024-09-20 11:44:13 +0200

    Added input type for apollo schema.

--- packages/apollo-api/src/server.js
+++ packages/apollo-api/src/server.js
@@ -7,18 +7,21 @@ const players = [
     id: "1",
     name: "John Doe",
     team: "Team 1",
+    age: 21,
     position: "Forward",
   },
   {
     id: "2",
     name: "Jane Doe",
     team: "Team 2",
+    age: 26,
     position: "Midfielder",
   },
   {
     id: "3",
     name: "Sam Doe",
     team: "Team 3",
+    age: 19,
     position: "Defender",
   },
 ];
@@ -26,20 +29,32 @@ const players = [
 export const createServer = async () => {
   const typeDefs = `#graphql
     type Player {
-        id: ID!
-        name: String!
-        team: String!
-        position: String!
+      id: ID!
+      name: String!
+      team: String!
+      position: String!
+    }
+
+    input PlayerQueryInput {
+      minAge: Int
     }
 
     type Query {
-        players: [Player!]!
+      players(q: PlayerQueryInput): [Player!]
     }
   `;
 
   const resolvers = {
     Query: {
-      players: () => players,
+      players: (_, args) => {
+        if (!args) {
+          return players;
+        }
+
+        const { minAge } = args.q;
+
+        return players.filter((player) => player.age >= minAge);
+      },
     },
   };
 
