716cfba5982cbc3f8739aa3608f65635dd857736
Anton Trafimovich <tonytrof@gmail.com>
2024-09-24 11:25:23 +0200

    Added basic union types.

--- packages/apollo-api/src/server.js
+++ packages/apollo-api/src/server.js
@@ -26,34 +26,99 @@ const players = [
   },
 ];
 
+// create coaches using structure described in the schema
+const coaches = [
+  {
+    id: "1",
+    name: "Coach 1",
+    team: "Team 1",
+  },
+  {
+    id: "2",
+    name: "Coach 2",
+    team: "Team 2",
+  },
+  {
+    id: "3",
+    name: "Coach 3",
+    team: "Team 3",
+  },
+];
+
 export const createServer = async () => {
   const typeDefs = `#graphql
+    enum Position {
+      Defender
+      Midfielder
+      Forward
+    }
+
+    
+    "A player in a team"
     type Player {
       id: ID!
       name: String!
+      """
+      Team the player belongs to. Maybe his **national** team or club team
+      """
       team: String!
-      position: String!
+      position: Position!
     }
+
+    "A coach in a team"
+    type Coach {
+      id: ID!
+      name: String!
+      team: String!
+    }
+
+    union TeamStuff = Player | Coach
 
     input PlayerQueryInput {
       minAge: Int
+      position: Position
     }
 
     type Query {
+      teamStuff(team: String!): [TeamStuff!]
       players(q: PlayerQueryInput): [Player!]
     }
   `;
 
   const resolvers = {
+    TeamStuff: {
+      __resolveType: (stuff) => {
+        // if (stuff.position) {
+        //   return 'Player';
+        // }
+
+        return 'Coach';
+      }
+    },
     Query: {
       players: (_, args) => {
-        if (!args) {
+        if (!args || !args.q) {
           return players;
         }
 
-        const { minAge } = args.q;
+        const { minAge, position } = args.q;
 
-        return players.filter((player) => player.age >= minAge);
+        return players.filter((player) => {
+          let pass = true;
+
+          if (minAge !== undefined && minAge !== null) {
+            pass = pass && player.age >= minAge;
+          }
+
+          if (position !== undefined && position !== null) {
+            pass = pass && player.position === position;
+          }
+
+          return pass;
+        });
+      },
+      teamStuff: (_, { team }) => {
+        return [{name: 'anton', gender: 'male'}];
       },
     },
   };
