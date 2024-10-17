041849a28c721300e3d0310b173d19f35b207f96
Anton Trafimovich <tonytrof@gmail.com>
2024-10-17 11:57:24 +0200

    Added interception routes to ordinary route.

---
+++ packages/next-frontend/src/app/feed/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import data from "../data/jokes.json";
+
+export default function Page() {
+  return (
+    <ul>
+      {data.jokes.map((joke, index) => (
+        <li key={index}>
+          <Link href={`/jokes/${index}`}>{index}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
